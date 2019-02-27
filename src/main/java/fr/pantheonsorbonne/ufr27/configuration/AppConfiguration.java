e2217e0792c96655c797268065825ca5f548366a
Nicolas Herbaut <nicolas.herbaut@univ-paris1.fr>
2019-02-27 17:03:07 +0100

    this fixes #5

---
+++ src/main/java/fr/pantheonsorbonne/ufr27/configuration/AppConfiguration.java
@@ -0,0 +1,13 @@
+package fr.pantheonsorbonne.ufr27.configuration;
+
+import com.google.inject.AbstractModule;
+
+public class AppConfiguration extends AbstractModule {
+
+	@Override
+	protected void configure() {
+
+	}
+
+
+}
