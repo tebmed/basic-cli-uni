92eadcfef402f6b93da1b82d9aa8a1508e43526d
Nicolas Herbaut <nicolas.herbaut@univ-paris1.fr>
2019-02-27 17:35:28 +0100

    this fixes #9

--- src/main/java/fr/pantheonsorbonne/ufr27/console/ConsoleEventParser.java
+++ src/main/java/fr/pantheonsorbonne/ufr27/console/ConsoleEventParser.java
@@ -37,6 +37,14 @@ public class ConsoleEventParser {
 				return factory.createStudentAction(matcher.group(1));
 			}
 		}
+		
+		{
+			Pattern pattern = Pattern.compile("^add course (.*)$");
+
+			if ((matcher = pattern.matcher(command)).matches()) {
+				return factory.createCourseAction(matcher.group(1));
+			}
+		}
 
 		throw new InvalidCommandException(command);
 
---
+++ src/main/java/fr/pantheonsorbonne/ufr27/entity/Course.java
@@ -0,0 +1,24 @@
+package fr.pantheonsorbonne.ufr27.entity;
+
+public class Course extends EducationEntity {
+
+	public Course(String name) {
+		super(name);
+	}
+
+
+	@Override
+	public int hashCode() {
+		return this.getName().hashCode();
+
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if (o instanceof Course) {
+			return ((Course) o).getName().equals(this.getName());
+		} else {
+			return false;
+		}
+	}
+}
